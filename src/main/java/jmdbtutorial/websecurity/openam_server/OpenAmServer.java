989e42c78b78a0330eb422eee4940cb862edcc38
Jim Barritt <jim@planet-ix.com>
2014-09-19 12:31:23 +0100

    [jmdb] Adding war path as a parameter

--- src/main/java/jmdbtutorial/websecurity/openam_server/OpenAmServer.java
+++ src/main/java/jmdbtutorial/websecurity/openam_server/OpenAmServer.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 import static java.lang.String.format;
 
@@ -16,23 +17,30 @@ public class OpenAmServer {
     private static final Logger log = LoggerFactory.getLogger(OpenAmServer.class);
 
     public static void main(String[] args) {
-        new OpenAmServer("openam-server", 9009, args[0]).start();
+        new OpenAmServer("openam-server", 9009, args[0], args[1]).start();
     }
 
     private String serverName;
     private final int httpPort;
     private final String webrootDir;
+    private final String openAmWarFilePath;
     private Server server;
 
 
-    public OpenAmServer(String serverName, int httpPort, String webrootDir) {
+    public OpenAmServer(String serverName, int httpPort, String webrootDir, String openAmWarFilePath) {
         this.serverName = serverName;
         this.httpPort = httpPort;
         this.webrootDir = webrootDir;
+        this.openAmWarFilePath = openAmWarFilePath;
     }
 
     public void start()  {
         try {
+            File openAmWar = new File(openAmWarFilePath);
+            if (!openAmWar.exists()) {
+                throw new FileNotFoundException(openAmWar.getAbsolutePath());
+            }
+
             log.info(format("Starting Server [%s] on port %d", serverName, httpPort));
 
             this.server = new Server(httpPort);
@@ -42,6 +50,7 @@ public class OpenAmServer {
             server.start();
 
             log.info((format("Static content is from [%s]", new File(webrootDir).getCanonicalPath())));
+            log.info((format("Open AM War from: [%s]", openAmWar.getAbsolutePath())));
             log.info(format("Server [%s] started @ http://localhost:%d", serverName, httpPort));
 
             server.join();
