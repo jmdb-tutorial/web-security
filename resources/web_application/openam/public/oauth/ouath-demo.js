fa22689985b87dc5d6adb0fae8ba39546dda8fc2
Jim Barritt <jim@planet-ix.com>
2015-12-15 20:04:20 +0000

    [jmdb] Got an access token back!

--- resources/web_application/openam/public/oauth/ouath-demo.js
+++ resources/web_application/openam/public/oauth/ouath-demo.js
@@ -1,8 +1,8 @@
 function initialiseLogger(outputElementId) {
     var outputElement = document.getElementById(outputElementId);
     console.log(outputElement);
-    return  {
-        info: function(text) {
+    return {
+        info: function (text) {
             var para = document.createElement("p");
             var textnode = document.createTextNode("[info] " + text);
             para.appendChild(textnode);
@@ -15,28 +15,93 @@ function initialiseLogger(outputElementId) {
 
 
 function invokeApiBtn_click(log) {
-    var clientAuthorisationRequest = {
-        clientId: "confidentialWebClient",
-        clientSecret: "oauthclient",
-        state: "foo",
-        redirectUrl: encodeURIComponent("http://websecurity.tutorial.com/oauth/authorization-grant.html"),
-        scope: "secrets"
-    };
 
     var redirectUrl = authorizationUrlTemplate.supplant(clientAuthorisationRequest)
 
     log.info("Redirect to " + redirectUrl);
-    window.location.href=redirectUrl;
+    window.location.href = redirectUrl;
 }
+
+function handleAccessTokenResponse(log, xhttp) {
+    var responseData = JSON.parse(xhttp.responseText);
+
+    log.info(JSON.stringify(responseData));
+}
+
+function getAccessToken(log, authorizationCode) {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status == 200) {
+                handleAccessTokenResponse(log, xhttp);
+            } else if (xhttp.status == 401) {
+                log.info("Not authorised! Login first")
+            } else {
+                log.info("Bad http response: " + xhttp.status);
+                log.info("Response: " + xhttp.responseText);
+            }
+        }
+    }
+
+
+    var userName = clientAuthorisationRequest['clientId'];
+    var password = clientAuthorisationRequest['clientSecret'];
+
+
+    xhttp.withCredentials = true;
+    xhttp.open("POST", accessTokenUrl, true);
+    xhttp.setRequestHeader("Authorization", "Basic " + btoa(userName + ":" + password));
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    var accessTokenRequest = {
+        authorizationCode: authorizationCode,
+        redirectUrl: clientAuthorisationRequest['redirectUrl'],
+        clientId: clientAuthorisationRequest['clientId']
+    }
+    var accessTokenBody = accessTokenBodyTemplate.supplant(accessTokenRequest)
+
+    log.info("curl --request POST --user \"" + userName + ":" + password +  "\"" +
+        " --data \"" + accessTokenBody + "\" " +
+        accessTokenUrl);
+
+
+    try {
+        xhttp.send(accessTokenBody);
+    } catch (e) {
+        alert(e);
+    }
+
+}
+
+function processRedirectResponse(log) {
+    var currentUrl = urlObject({'url': window.location});
+
+    var authorisationCode = currentUrl.parameters['code'];
+    if (authorisationCode) {
+        log.info("Authorisaztion code: " + authorisationCode);
+        getAccessToken(log, authorisationCode);
+    }
+}
+
 
 function initialiseApp() {
     var log = initialiseLogger("output");
 
-    log.info("Demo of openam ouath request");
+    log.info("Demo of openam oauth code grant flow.");
+
+    log.info("Authorisation Request Parameters:")
+    for (var key in clientAuthorisationRequest) {
+        log.info(key + ": " + clientAuthorisationRequest[key]);
+    }
+    log.info("Authorisation URI: " + authorizationUrlTemplate.supplant(clientAuthorisationRequest));
 
     document.getElementById("invokeApiBtn").addEventListener("click", function () {
         invokeApiBtn_click(log);
     });
+
+    processRedirectResponse(log);
+
 
 }
 
@@ -53,11 +118,26 @@ String.prototype.supplant = function (o) {
     );
 };
 
+var clientAuthorisationRequest = {
+    clientId: "confidentialWebClient",
+    clientSecret: "oauthclient",
+    state: "foo",
+    redirectUrl: encodeURIComponent("http://websecurity.tutorial.com/oauth/authorization-grant.html"),
+    scope: "secrets"
+};
+
 var authorizationUrlTemplate = "http://loan.example.com:9009/openam/oauth2/authorize" +
-                                "?response_type=code" +
-                                "&client_id={clientId}" +
-                                "&client_secret={clientSecret}" +
-                                "&state=foo={state}" +
-                                "&redirect_uri={redirectUrl}" +
-                                "&scope={scope}";
+    "?response_type=code" +
+    "&client_id={clientId}" +
+    "&client_secret={clientSecret}" +
+    "&state=foo={state}" +
+    "&redirect_uri={redirectUrl}" +
+    "&scope={scope}";
+
+var accessTokenUrl = "http://loan.example.com:9009/openam/oauth2/access_token";
+var accessTokenBodyTemplate = "grant_type=authorization_code" +
+    "&code={authorizationCode}" +
+    "&redirect_uri={redirectUrl}" +
+    "&clientId={clientId}";
+
 window.onload = initialiseApp;
